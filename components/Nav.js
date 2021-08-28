82ba08d164179735572f6a3ee1dd21be355c35f9
amchicken <yooradet.khamon@hotmail.com>
2021-08-28 20:09:51 +0700

    fix error

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { useState, useContext, useRef, useEffect } from "react";
 import { UserContext } from "@lib/UserContext";
 import { LogoutButton } from "@components/Auth";
-import { FaBars } from 'react-icons/fa'
+import { FaBars } from "react-icons/fa";
 
 export default function Nav() {
   const [usermenu, setUserMenu] = useState(false);
@@ -11,48 +11,62 @@ export default function Nav() {
   const navRef = useRef(null);
 
   function clickOutsideNav(ref) {
-    useEffect(() => {
-        /**
-         * Alert if clicked on outside of element
-         */
-        function handleClickOutside(event) {
-            if (ref.current && !ref.current.contains(event.target)) {
-                setUserMenu(false)
-            }
-        }
-  
-        // Bind the event listener
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            // Unbind the event listener on clean up
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [ref]);
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setUserMenu(false);
+      }
+    }
   }
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
 
-  clickOutsideNav(navRef)
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+
+  clickOutsideNav(navRef);
 
   return (
     <nav className="nav">
-      <div className="nav__left-side"
-       onClick={() => setUserMenu(true)}
-      >
+      <div className="nav__left-side" onClick={() => setUserMenu(true)}>
         <FaBars />
       </div>
-      <div className={usermenu ? "nav__float show" : "nav__float hidden"} ref={navRef}>
+      <div
+        className={usermenu ? "nav__float show" : "nav__float hidden"}
+        ref={navRef}
+      >
         <div>
           <Image
             src={user.photoURL}
-            width={50}
-            height={50}
+            width={100}
+            height={100}
             alt="profile-image"
           />
-          <h2>{user.displayName}</h2>
+          <h2>@ {user.displayName}</h2>
         </div>
         <ul>
-          <li>Profile</li>
+          <li>
+            <div>
+              <Image
+                src="http://via.placeholder.com/50"
+                height={50}
+                width={50}
+                alt="logo"
+              />
+              Profile
+            </div>
+          </li>
           <li>Daliy challenges</li>
           <li>Leaderboard</li>
+          <li>Repository</li>
+          <li>Private Session</li>
+          <li>Serch icon</li>
         </ul>
         <LogoutButton />
       </div>
