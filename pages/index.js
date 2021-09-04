2554bdb98a7ffc048053235b2446e878bddc7c50
ThisGameZR <timezazare@gmail.com>
2021-09-04 16:33:48 +0700

    NavBar CSS

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,12 @@ import { LogoutButton } from "@components/Auth";
 import Nav from "@components/Nav";
 
 export default function Home() {
-  const user = useContext(UserContext);
+    const user = useContext(UserContext);
 
-  return (
-    <>
-      <Nav />
-      <div>
-        <h1>Welcome {user.displayName || "New user"} to dailyPoisson</h1>
-        <LogoutButton />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Nav />
+            <div></div>
+        </>
+    );
 }
