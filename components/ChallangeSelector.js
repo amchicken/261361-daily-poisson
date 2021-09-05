aa3e17234c071145d912ca421e07c2bba898d5f9
ThisGameZR <timezazare@gmail.com>
2021-09-06 00:09:49 +0700

    Logout box

--- components/ChallangeSelector.js
+++ components/ChallangeSelector.js
@@ -4,56 +4,59 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 
 export default function ChallangeSelector() {
-  const router = useRouter();
-  const [snapshot, loading] = useChallangeData();
+    const router = useRouter();
+    const [snapshot, loading] = useChallangeData();
 
-  if (loading) {
-    return <div style={{ display: "flex" }}>loading...</div>;
-  }
+    if (loading) {
+        return <div style={{ display: "flex" }}>loading...</div>;
+    }
 
-  const createChallenge = () => {
-    router.push("/Challenge/create");
-  };
+    const createChallenge = () => {
+        router.push("/Challenge/create");
+    };
 
-  return (
-    <div className="container__right">
-      <div className="container__right__header">
-        <span style={{ fontWeight: 600 }}>HOME</span>
-        <span>TOP PLAY</span>
-        <div>
-          LOAD NEW CHALLENGES{"  "}
-          <IoReload style={{ color: "#48f36e" }} />
+    return (
+        <div className="container__right">
+            <div className="container__right__header">
+                <span style={{ fontWeight: 600 }}>HOME</span>
+                <span>TOP PLAY</span>
+                <div>
+                    LOAD NEW CHALLENGES{"  "}
+                    <IoReload style={{ color: "#48f36e" }} />
+                </div>
+            </div>
+            <div className="container__right__content">
+                {snapshot.map((doc) => (
+                    <div
+                        key={doc.id}
+                        className="container__right__content__card"
+                    >
+                        <div className="container__right__content__card__show">
+                            <Image
+                                src={doc.thumbnail}
+                                width={100}
+                                height={100}
+                                quality={100}
+                                alt="thumbnail"
+                            />
+                            <div>
+                                <p>{doc.level.toUpperCase()}</p>
+                                <h4>
+                                    {doc.question} Question{" "}
+                                    {doc.question > 1 ? s : ""}
+                                </h4>
+                                <span>{doc.category}</span>
+                            </div>
+                        </div>
+                        <div className="container__right__content__card__float">
+                            <Image src="/img/btn.png" layout="fill" />
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="container__right__footer">
+                <button onClick={createChallenge}>REQUEST YOUR {"Q&A"}</button>
+            </div>
         </div>
-      </div>
-      <div className="container__right__content">
-        {snapshot.map((doc) => (
-          <div key={doc.id} className="container__right__content__card">
-            <div className="container__right__content__card__show">
-              <Image
-                src={doc.thumbnail}
-                width={100}
-                height={100}
-                quality={100}
-                alt="thumbnail"
-              />
-              <div>
-                <p>{doc.name}</p>
-                <h4>{doc.level.toUpperCase()}</h4>
-                <span>
-                  <b>{doc.question}</b> Question
-                </span>
-                <span>{doc.category}</span>
-              </div>
-            </div>
-            <div className="container__right__content__card__float">
-              <Image src="/img/btn.png" layout="fill" />
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="container__right__footer">
-        <button onClick={createChallenge}>REQUEST YOUR {"Q&A"}</button>
-      </div>
-    </div>
-  );
+    );
 }
--- components/Nav.js
+++ components/Nav.js
@@ -4,81 +4,56 @@ import { useState, useContext, useRef, useEffect } from "react";
 import { UserContext } from "@lib/UserContext";
 import { LogoutButton } from "@components/Auth";
 import { FaBars, FaSearch } from "react-icons/fa";
-import { useClickOutSide } from "@lib/useClickOutSide";
 import { useMediaQuery } from "react-responsive";
 import { auth } from "@lib/firebase";
 
 export default function Nav() {
-  const isMobile = useMediaQuery({ maxWidth: 767 });
-  const [usermenu, setUserMenu] = useState(false);
-  const user = useContext(UserContext);
-  const navRef = useClickOutSide(() => {
-    setUserMenu(false);
-  });
+    const user = useContext(UserContext);
+    const [showLogOut, setShowLogOut] = useState(false);
 
-  if (isMobile) {
     return (
-      <nav className="nav">
-        <div className="nav__float show" ref={navRef}>
-          <div className="nav__float__top">
-            <Image
-              src={user.photoURL}
-              width={100}
-              height={100}
-              alt="profile-image"
-            />
-            <span>{user.displayName}</span>
-            <div className="nav__float__top line"></div>
-          </div>
-          <ul className="nav__float__list">
-            <Image src="/img/logo.png" width="90" height="64" />
-            <li>PROFILE</li>
-            <li>DAILY CHALLENGES</li>
-            <li>LEADERBOARD</li>
-            <li>REPOSITORY</li>
-            <li>PRIVATE SESSION</li>
-            <li>
-              <FaSearch />
-            </li>
-            <span>
-              CONTACT | ABOUT | HELP CENTER |{" "}
-              <b onClick={() => auth.signOut()}>{"   "}Logout</b>
-            </span>
-          </ul>
-        </div>
-      </nav>
+        <nav className="nav">
+            <div className="nav__float show">
+                <div className="nav__float__top">
+                    <Image
+                        src={user.photoURL}
+                        width={100}
+                        height={100}
+                        alt="profile-image"
+                    />
+                    {showLogOut ? (
+                        <div className="logout-box">
+                            <div>
+                                <div>{user.displayName}</div>
+                                <span>{user.email}</span>
+                            </div>
+                            <button onClick={() => auth.signOut()}>
+                                SIGN OUT
+                            </button>
+                        </div>
+                    ) : (
+                        <span onClick={() => setShowLogOut(!showLogOut)}>
+                            {user.displayName}
+                        </span>
+                    )}
+                    <div className="nav__float__top line"></div>
+                </div>
+                <ul
+                    className="nav__float__list"
+                    onClick={() => setShowLogOut(false)}
+                >
+                    <Image src="/img/logo.png" width="90" height="64" />
+                    <li>PROFILE</li>
+                    <li>DAILY CHALLENGES</li>
+                    <li>LEADERBOARD</li>
+                    <li>REPOSITORY</li>
+                    <li>PRIVATE SESSION</li>
+                    <li>
+                        <FaSearch />
+                    </li>
+                    <span>CONTACT | ABOUT | HELP CENTER |</span>
+                </ul>
+            </div>
+        </nav>
     );
-  }
-
-  return (
-    <nav className="nav">
-      <div className="nav__float show" ref={navRef}>
-        <div className="nav__float__top">
-          <Image
-            src={user.photoURL}
-            width={100}
-            height={100}
-            alt="profile-image"
-          />
-          <span>{user.displayName}</span>
-          <div className="nav__float__top line"></div>
-        </div>
-        <ul className="nav__float__list">
-          <Image src="/img/logo.png" width="90" height="64" />
-          <li>PROFILE</li>
-          <li>DAILY CHALLENGES</li>
-          <li>LEADERBOARD</li>
-          <li>REPOSITORY</li>
-          <li>PRIVATE SESSION</li>
-          <li>
-            <FaSearch style={{ color: "#48F36E" }} />
-          </li>
-          <span>
-            CONTACT | ABOUT | HELP CENTER |
-            <b onClick={() => auth.signOut()}>{"   "}Logout</b>
-          </span>
-        </ul>
-      </div>
-    </nav>
-  );
 }
