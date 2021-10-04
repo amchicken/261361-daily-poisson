4bd950c7c0d4b0447e41c26f329b3b60332166fb
ThisGameZR <timezazare@gmail.com>
2021-10-04 18:25:00 +0700

    Change route

--- pages/Challenge/[cid]/index.js
+++ pages/Challenge/[cid]/index.js
@@ -7,102 +7,109 @@ import Image from "next/image";
 import { toast } from "react-hot-toast";
 
 export default function ChallengeIndex() {
-  const router = useRouter();
-  const { cid } = router.query;
+    const router = useRouter();
+    const { cid } = router.query;
 
-  const [data, setData] = useState();
-  const [notFound, setNotFound] = useState(false);
-  const [doc, loading] = useDocumentOnce(
-    firestore.collection("challenges").doc(cid)
-  );
-  const [owner, setOwner] = useState();
+    const [data, setData] = useState();
+    const [notFound, setNotFound] = useState(false);
+    const [doc, loading] = useDocumentOnce(
+        firestore.collection("challenges").doc(cid)
+    );
+    const [owner, setOwner] = useState();
 
-  useEffect(() => {
-    const prep = async () => {
-      if (!loading) {
-        if (doc.exists) {
-          const target = doc.data();
-          setData(target);
-          if (target.createdBy) {
-            setOwner(
-              (
-                await firestore
-                  .collection("usernames")
-                  .doc(target.createdBy)
-                  .get()
-              ).data()
-            );
-          }
-        } else setNotFound(true);
-      }
-    };
+    useEffect(() => {
+        const prep = async () => {
+            if (!loading) {
+                if (doc.exists) {
+                    const target = doc.data();
+                    setData(target);
+                    if (target.createdBy) {
+                        setOwner(
+                            (
+                                await firestore
+                                    .collection("usernames")
+                                    .doc(target.createdBy)
+                                    .get()
+                            ).data()
+                        );
+                    }
+                } else setNotFound(true);
+            }
+        };
 
-    prep();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loading]);
+        prep();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [loading]);
 
-  return (
-    <div className="container">
-      <Nav />
-      <div style={{ backgroundColor: "#fff" }}>
-        {loading
-          ? "loading.."
-          : notFound
-          ? "NOT FOUND CHALLENGE"
-          : data && (
-              <>
-                {data.name}
-                <br />
-                <Image
-                  src={data.thumbnail || "/notfound.png"}
-                  width={100}
-                  height={100}
-                  alt="challenge thumbnail"
-                />
-                <br />
-                level {data.level}
-                <br />
-                estimatepoint <b>500</b>
-                <br />
-                item {data.question}
-                <br />
-                category {data.category}
-                <br />
-                description {data.description}
-                <br />
-                tags :{" "}
-                {data.tags.map((tag, idx) => (
-                  <i key={idx}>{tag}</i>
-                ))}
-                <br />
-                <br />
-                <br />
-                The Creator:{" "}
-                {owner && (
-                  <>
-                    <Image
-                      src={owner.photoURL || "/notfound.png"}
-                      width={100}
-                      height={100}
-                      alt="createorphoto"
-                    />
-                    @{owner.username} <br />
-                    Publisthed Date:{data.createdAt?.toDate().toJSON()}
-                    <br />
-                    More Form @{owner.username}
-                  </>
-                )}
-                {data.played.includes(auth.currentUser.uid) ? (
-                  "ALREDY play this challenge"
-                ) : (
-                  <button onClick={() => router.push(`/challenge/${cid}/play`)}>
-                    Play
-                  </button>
-                )}
-              </>
-            )}
-      </div>
-      <div className="container__footer">DAILYPOISSON 2021 | SITE</div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <Nav />
+            <div style={{ backgroundColor: "#fff" }}>
+                {loading
+                    ? "loading.."
+                    : notFound
+                    ? "NOT FOUND CHALLENGE"
+                    : data && (
+                          <>
+                              {data.name}
+                              <br />
+                              <Image
+                                  src={data.thumbnail || "/notfound.png"}
+                                  width={100}
+                                  height={100}
+                                  alt="challenge thumbnail"
+                              />
+                              <br />
+                              level {data.level}
+                              <br />
+                              estimatepoint <b>500</b>
+                              <br />
+                              item {data.question}
+                              <br />
+                              category {data.category}
+                              <br />
+                              description {data.description}
+                              <br />
+                              tags :{" "}
+                              {data.tags.map((tag, idx) => (
+                                  <i key={idx}>{tag}</i>
+                              ))}
+                              <br />
+                              <br />
+                              <br />
+                              The Creator:{" "}
+                              {owner && (
+                                  <>
+                                      <Image
+                                          src={
+                                              owner.photoURL || "/notfound.png"
+                                          }
+                                          width={100}
+                                          height={100}
+                                          alt="createorphoto"
+                                      />
+                                      @{owner.username} <br />
+                                      Publisthed Date:
+                                      {data.createdAt?.toDate().toJSON()}
+                                      <br />
+                                      More Form @{owner.username}
+                                  </>
+                              )}
+                              {data.played.includes(auth.currentUser.uid) ? (
+                                  "ALREDY play this challenge"
+                              ) : (
+                                  <button
+                                      onClick={() =>
+                                          router.push(`/Challenge/${cid}/play`)
+                                      }
+                                  >
+                                      Play
+                                  </button>
+                              )}
+                          </>
+                      )}
+            </div>
+            <div className="container__footer">DAILYPOISSON 2021 | SITE</div>
+        </div>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,65 +5,78 @@ import Link from "next/link";
 import { useChallangeData } from "@lib/useChallangeData";
 
 export default function Home() {
-  const [snapshot, loading] = useChallangeData();
-  return (
-    <>
-      <div className="container">
-        <Nav />
-        <div className="container__right">
-          {loading ? (
-            <div>loading...</div>
-          ) : (
-            <>
-              <div className="container__right__header">
-                <span style={{ fontWeight: 600 }}>HOME</span>
-                <span>TOP PLAY</span>
-                <div>
-                  LOAD NEW CHALLENGES{"  "}
-                  <IoReload style={{ color: "#48f36e" }} />
+    const [snapshot, loading] = useChallangeData();
+    return (
+        <>
+            <div className="container">
+                <Nav />
+                <div className="container__right">
+                    {loading ? (
+                        <div>loading...</div>
+                    ) : (
+                        <>
+                            <div className="container__right__header">
+                                <span style={{ fontWeight: 600 }}>HOME</span>
+                                <span>TOP PLAY</span>
+                                <div>
+                                    LOAD NEW CHALLENGES{"  "}
+                                    <IoReload style={{ color: "#48f36e" }} />
+                                </div>
+                            </div>
+                            <div className="container__right__content">
+                                {snapshot.map((doc) => (
+                                    <Link
+                                        href={`challenge/${doc.id}`}
+                                        passHref
+                                        key={doc.id}
+                                    >
+                                        <div className="container__right__content__card">
+                                            <div className="container__right__content__card__show">
+                                                <Image
+                                                    src={
+                                                        doc.thumbnail ||
+                                                        "/notfound.png"
+                                                    }
+                                                    width={100}
+                                                    height={100}
+                                                    quality={100}
+                                                    alt="thumbnail"
+                                                />
+                                                <div>
+                                                    <p>
+                                                        {doc.level.toUpperCase()}
+                                                    </p>
+                                                    <h4>
+                                                        {doc.question} Items
+                                                    </h4>
+                                                    <span>{doc.category}</span>
+                                                </div>
+                                            </div>
+                                            <div className="container__right__content__card__float">
+                                                <div>
+                                                    <Image
+                                                        src="/img/btn.png"
+                                                        layout="fill"
+                                                        alt="button"
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                ))}
+                            </div>
+                            <div className="container__right__footer">
+                                <Link href="/Challenge/create" passHref>
+                                    <button>REQUEST YOUR {"Q&A"}</button>
+                                </Link>
+                            </div>
+                        </>
+                    )}
                 </div>
-              </div>
-              <div className="container__right__content">
-                {snapshot.map((doc) => (
-                  <Link href={`challenge/${doc.id}`} passHref key={doc.id}>
-                    <div className="container__right__content__card">
-                      <div className="container__right__content__card__show">
-                        <Image
-                          src={doc.thumbnail || "/notfound.png"}
-                          width={100}
-                          height={100}
-                          quality={100}
-                          alt="thumbnail"
-                        />
-                        <div>
-                          <p>{doc.level.toUpperCase()}</p>
-                          <h4>{doc.question} Items</h4>
-                          <span>{doc.category}</span>
-                        </div>
-                      </div>
-                      <div className="container__right__content__card__float">
-                        <div>
-                          <Image
-                            src="/img/btn.png"
-                            layout="fill"
-                            alt="button"
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  </Link>
-                ))}
-              </div>
-              <div className="container__right__footer">
-                <Link href="/challenge/create" passHref>
-                  <button>REQUEST YOUR {"Q&A"}</button>
-                </Link>
-              </div>
-            </>
-          )}
-        </div>
-        <div className="container__footer">DAILYPOISSON 2021 | SITE</div>
-      </div>
-    </>
-  );
+                <div className="container__footer">
+                    DAILYPOISSON 2021 | SITE
+                </div>
+            </div>
+        </>
+    );
 }
