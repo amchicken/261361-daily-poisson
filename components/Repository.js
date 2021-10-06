939a254c82a03f6aa6215efd33e4fbc6875328fb
amchicken <yooradet.khamon@hotmail.com>
2021-10-06 22:21:15 +0700

    email & displayname fix

--- components/Repository.js
+++ components/Repository.js
@@ -11,156 +11,148 @@ import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 const LIMIT = 6;
 
 export default function Repository({ sort = "date" }) {
-    const router = useRouter();
-    const [data, setData] = useState([]);
-    const [first, setFirst] = useState();
-    const [last, setLast] = useState();
-    const [firstDocment, setFirstDocment] = useState();
-    const [lastDocment, setLastDocment] = useState();
-    const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const [data, setData] = useState([]);
+  const [first, setFirst] = useState();
+  const [last, setLast] = useState();
+  const [firstDocment, setFirstDocment] = useState();
+  const [lastDocment, setLastDocment] = useState();
+  const [loading, setLoading] = useState(true);
 
-    const getData = async (next = true) => {
-        setLoading(true);
-        const ref = firestore.collection("challenges");
+  const getData = async (next = true) => {
+    setLoading(true);
+    const ref = firestore.collection("challenges");
 
-        console.log(sort);
-        let query;
+    console.log(sort);
+    let query;
 
-        if (firstDocment || lastDocment) {
-            if (next) {
-                if (sort === "date") {
-                    query = ref
-                        .orderBy("createdAt", "desc")
-                        .startAfter(lastDocment.createdAt)
-                        .limit(LIMIT);
-                } else {
-                    query = ref
-                        .orderBy("play", "desc")
-                        .startAfter(lastDocment.play)
-                        .limit(LIMIT);
-                }
-            } else {
-                if (sort === "date") {
-                    query = ref
-                        .orderBy("createdAt", "asc")
-                        .startAfter(firstDocment.createdAt)
-                        .limit(LIMIT);
-                } else {
-                    query = ref
-                        .orderBy("play", "asc")
-                        .startAfter(firstDocment.play)
-                        .limit(LIMIT);
-                }
-            }
+    if (firstDocment || lastDocment) {
+      if (next) {
+        if (sort === "date") {
+          query = ref
+            .orderBy("createdAt", "desc")
+            .startAfter(lastDocment.createdAt)
+            .limit(LIMIT);
         } else {
-            if (sort === "date")
-                query = ref.orderBy("createdAt", "desc").limit(LIMIT);
-            else query = ref.orderBy("play", "desc").limit(LIMIT);
+          query = ref
+            .orderBy("play", "desc")
+            .startAfter(lastDocment.play)
+            .limit(LIMIT);
         }
-        const firestoreData = (await query.get()).docs.map((doc) => {
-            return { ...doc.data(), id: doc.id };
-        });
-
-        //REVERS ASC TO DESC
-        if (!next) firestoreData.reverse();
-
-        setData(firestoreData);
-
-        setFirstDocment(firestoreData[0]);
-        setLastDocment(firestoreData[firestoreData.length - 1]);
-
-        //FIRSTIME ONLY
-        if (!first) setFirst(firestoreData[0]);
-        if (!last) {
-            const last = (
-                await ref.orderBy("createdAt", "asc").limit(1).get()
-            ).docs.map((doc) => {
-                return { ...doc.data(), id: doc.id };
-            });
-            setLast(last[0]);
+      } else {
+        if (sort === "date") {
+          query = ref
+            .orderBy("createdAt", "asc")
+            .startAfter(firstDocment.createdAt)
+            .limit(LIMIT);
+        } else {
+          query = ref
+            .orderBy("play", "asc")
+            .startAfter(firstDocment.play)
+            .limit(LIMIT);
         }
+      }
+    } else {
+      if (sort === "date")
+        query = ref.orderBy("createdAt", "desc").limit(LIMIT);
+      else query = ref.orderBy("play", "desc").limit(LIMIT);
+    }
+    const firestoreData = (await query.get()).docs.map((doc) => {
+      return { ...doc.data(), id: doc.id };
+    });
 
-        setLoading(false);
+    //REVERS ASC TO DESC
+    if (!next) firestoreData.reverse();
+
+    setData(firestoreData);
+
+    setFirstDocment(firestoreData[0]);
+    setLastDocment(firestoreData[firestoreData.length - 1]);
+
+    //FIRSTIME ONLY
+    if (!first) setFirst(firestoreData[0]);
+    if (!last) {
+      const last = (
+        await ref.orderBy("createdAt", "asc").limit(1).get()
+      ).docs.map((doc) => {
+        return { ...doc.data(), id: doc.id };
+      });
+      setLast(last[0]);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
+
+    return () => {
+      console.log("RETURN" + sort);
+      setFirst();
+      setLast();
+      setFirstDocment();
+      setLastDocment();
+      setData([]);
     };
+  }, []);
 
-    useEffect(() => {
-        getData();
-
-        return () => {
-            console.log("RETURN" + sort);
-            setFirst();
-            setLast();
-            setFirstDocment();
-            setLastDocment();
-            setData([]);
-        };
-    }, []);
-
-    return loading ? (
-        <div>Loding. ...</div>
-    ) : (
+  return loading ? (
+    <div>Loding. ...</div>
+  ) : (
+    <>
+      {data.map((doc) => (
+        <React.Fragment key={doc.id}>
+          <div className="container__right__content__card">
+            <div className="container__right__content__card__show">
+              <Image
+                src={doc.thumbnail || "/notfound.png"}
+                width={100}
+                height={100}
+                quality={100}
+                alt="thumbnail"
+              />
+              <div>
+                <p>{doc.name}</p>
+                <h4>{doc.question} Items</h4>
+                <span>{doc.category}</span>
+                <h4>{doc.level.toUpperCase()}</h4>
+              </div>
+            </div>
+            <div className="container__right__content__card__float">
+              {doc.played?.includes(auth.currentUser.uid) ? (
+                <div>
+                  <AiFillStop
+                    style={{
+                      cursor: "not-allowed",
+                      backgroundColor: "#ef8c8c",
+                    }}
+                  />
+                </div>
+              ) : (
+                <div>
+                  <AiFillPlayCircle
+                    onClick={() => router.push(`challenge/${doc.id}`)}
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+        </React.Fragment>
+      ))}
+      {!loading && (
         <>
-            {data.map((doc) => (
-                <React.Fragment key={doc.id}>
-                    <div className="container__right__content__card">
-                        <div className="container__right__content__card__show">
-                            <Image
-                                src={doc.thumbnail || "/notfound.png"}
-                                width={100}
-                                height={100}
-                                quality={100}
-                                alt="thumbnail"
-                            />
-                            <div>
-                                <p>{doc.name}</p>
-                                <h4>{doc.question} Items</h4>
-                                <span>{doc.category}</span>
-                                <h4>{doc.level.toUpperCase()}</h4>
-                            </div>
-                        </div>
-                        <div className="container__right__content__card__float">
-                            {doc.played?.includes(auth.currentUser.uid) ? (
-                                <div>
-                                    <AiFillStop
-                                        style={{
-                                            cursor: "not-allowed",
-                                            backgroundColor: "#ef8c8c",
-                                        }}
-                                    />
-                                </div>
-                            ) : (
-                                <div>
-                                    <AiFillPlayCircle
-                                        onClick={() =>
-                                            router.push(`Challenge/${doc.id}`)
-                                        }
-                                    />
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </React.Fragment>
-            ))}
-            {!loading && (
-                <>
-                    {firstDocment?.id === first?.id ? null : (
-                        <button
-                            className="back-btn"
-                            onClick={() => getData(false)}
-                        >
-                            <FaArrowCircleLeft />
-                        </button>
-                    )}
-                    {lastDocment?.id === last?.id ? null : (
-                        <button
-                            className="next-btn"
-                            onClick={() => getData(true)}
-                        >
-                            <FaArrowCircleRight />
-                        </button>
-                    )}
-                </>
-            )}
+          {firstDocment?.id === first?.id ? null : (
+            <button className="back-btn" onClick={() => getData(false)}>
+              <FaArrowCircleLeft />
+            </button>
+          )}
+          {lastDocment?.id === last?.id ? null : (
+            <button className="next-btn" onClick={() => getData(true)}>
+              <FaArrowCircleRight />
+            </button>
+          )}
         </>
-    );
+      )}
+    </>
+  );
 }
--- pages/Challenge/[cid]/play.js
+++ pages/Challenge/[cid]/play.js
@@ -1,133 +1,136 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import Image from "next/image";
 import _ from "lodash";
 import { POINTS } from "@lib/constants";
 import { useRouter } from "next/router";
 import {
-    auth,
-    firestore,
-    serverTimestamp,
-    arrayUnion,
-    Increment,
+  auth,
+  firestore,
+  serverTimestamp,
+  arrayUnion,
+  Increment,
 } from "@lib/firebase";
 import {
-    useCollectionDataOnce,
-    useDocumentDataOnce,
+  useCollectionDataOnce,
+  useDocumentDataOnce,
 } from "react-firebase-hooks/firestore";
+import { UserContext } from "@lib/UserContext";
 // kPxbkD5hWipVYVlkUyFY
 
 export default function Submitx() {
-    const router = useRouter();
-    const { cid } = router.query;
-    const [timer, setTimer] = useState();
-    const [index, setIndex] = useState();
-    const [current, setCurrent] = useState();
-    //   const [loading, setLoading] = useState(true);
-    const [document, loading] = useCollectionDataOnce(
-        firestore.collection("challenges").doc(cid).collection("questions")
-    );
-    const [challengeDetail, detailLoading] = useDocumentDataOnce(
-        firestore.collection("challenges").doc(cid)
-    );
+  const router = useRouter();
+  const { cid } = router.query;
+  const { user } = useContext(UserContext);
+  const [timer, setTimer] = useState();
+  const [index, setIndex] = useState();
+  const [current, setCurrent] = useState();
+  const [correctness, setCorrectness] = useState(0);
+  //   const [loading, setLoading] = useState(true);
+  const [document, loading] = useCollectionDataOnce(
+    firestore.collection("challenges").doc(cid).collection("questions")
+  );
+  const [challengeDetail, detailLoading] = useDocumentDataOnce(
+    firestore.collection("challenges").doc(cid)
+  );
 
-    const [data, setData] = useState([]);
-    const [answer, setAnswer] = useState("");
-    const [score, setScore] = useState(0);
-    const [finished, setFinished] = useState(false);
+  const [data, setData] = useState([]);
+  const [answer, setAnswer] = useState("");
+  const [score, setScore] = useState(0);
+  const [finished, setFinished] = useState(false);
 
-    useEffect(() => {
-        let myInterval = setInterval(() => {
-            if (!loading && timer > 0) setTimer((time) => time - 1);
-        }, 1000);
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (!loading && timer > 0) setTimer((time) => time - 1);
+    }, 1000);
 
-        return () => {
-            clearInterval(myInterval);
-        };
-    });
+    return () => {
+      clearInterval(myInterval);
+    };
+  });
 
-    useEffect(() => {
-        if (!loading) {
-            setIndex(-1);
-            setTimer(4);
-            setFinished(false);
-            setData(_.shuffle(document));
-        }
-    }, [loading]);
+  useEffect(() => {
+    if (!loading) {
+      setIndex(-1);
+      setTimer(4);
+      setFinished(false);
+      setData(_.shuffle(document));
+    }
+  }, [loading]);
 
-    useEffect(() => {
-        if (timer === 0 && index < data.length) setIndex((idx) => idx + 1);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [timer]);
+  useEffect(() => {
+    if (timer === 0 && index < data.length) setIndex((idx) => idx + 1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [timer]);
 
-    useEffect(() => {
-        if (index > -1) {
-            if (index < data.length) {
-                setCurrent(data[index]);
-                setTimer(data[index].time);
-            } else {
-                setCurrent();
-                setTimer();
-            }
-        }
-        if (index === data.length) {
-            setFinished(true);
-            updateScore();
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [index]);
+  useEffect(() => {
+    if (index > -1) {
+      if (index < data.length) {
+        setCurrent(data[index]);
+        setTimer(data[index].time);
+      } else {
+        setCurrent();
+        setTimer();
+      }
+    }
+    if (index === data.length) {
+      setFinished(true);
+      updateScore();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [index]);
 
-    useEffect(() => {
-        if (
-            typeof current !== "undefined" &&
-            answer !== "" &&
-            answer === current.correct
-        )
-            setScore((old) => old + POINTS(current.level));
-
-        if (index < data.length) setIndex((idx) => idx + 1);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [answer]);
-
-    async function updateScore() {
-        const batch = firestore.batch();
-        const userRef = firestore
-            .collection("usernames")
-            .doc(auth.currentUser.uid);
-        const historyRef = userRef.collection("history").doc(cid);
-        const challengeRef = firestore.collection("challenges").doc(cid);
-
-        if (challengeDetail.played.includes(auth.currentUser.uid)) {
-            console.log("ALRE PLAY");
-        } else {
-            if (typeof challengeDetail.played !== "undefined")
-                batch.update(challengeRef, {
-                    played: arrayUnion(auth.currentUser.uid),
-                    play: Increment(1),
-                });
-            else
-                batch.update(challengeRef, {
-                    played: [auth.currentUser.uid],
-                    play: Increment(1),
-                });
-
-            batch.update(userRef, {
-                points: Increment(score),
-                lastSeen: serverTimestamp(),
-            });
-
-            batch.set(historyRef, {
-                name: challengeDetail.name,
-                points: score,
-                createdAt: serverTimestamp(),
-            });
-
-            batch.commit();
-        }
+  useEffect(() => {
+    if (
+      typeof current !== "undefined" &&
+      answer !== "" &&
+      answer === current.correct
+    ) {
+      setCorrectness((old) => old + 1);
+      setScore((old) => old + POINTS(current.level));
     }
 
-    return (
-        <div className="App">
-            {/* <div>
+    if (index < data.length) setIndex((idx) => idx + 1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [answer]);
+
+  async function updateScore() {
+    const batch = firestore.batch();
+    const userRef = firestore.collection("usernames").doc(auth.currentUser.uid);
+    const historyRef = userRef.collection("history").doc(cid);
+    const challengeRef = firestore.collection("challenges").doc(cid);
+
+    if (challengeDetail.played.includes(auth.currentUser.uid)) {
+      console.log("ALRE PLAY");
+    } else {
+      if (typeof challengeDetail.played !== "undefined")
+        batch.update(challengeRef, {
+          played: arrayUnion(auth.currentUser.uid),
+          play: Increment(1),
+        });
+      else
+        batch.update(challengeRef, {
+          played: [auth.currentUser.uid],
+          play: Increment(1),
+        });
+
+      batch.update(userRef, {
+        points: Increment(score),
+        lastSeen: serverTimestamp(),
+      });
+
+      batch.set(historyRef, {
+        name: challengeDetail.name,
+        points: score,
+        createdAt: serverTimestamp(),
+      });
+
+      batch.commit();
+    }
+  }
+
+  return (
+    <div className="App">
+      {/* <div>
         <span>Debug</span>
         <br />
         <span>timer : {timer}</span>
@@ -139,91 +142,83 @@ export default function Submitx() {
         <span>current : {JSON.stringify(current)}</span>
         <br />
       </div> */}
-            <div className="playCard">
-                <span onClick={() => router.push(`/`)}>&#10006;</span>
-                <div className="playCard__header">
-                    <h1>{detailLoading ? null : challengeDetail.name}</h1>
-                </div>
-                <div className="playCard__body">
-                    {loading ? (
-                        <div className="question--list">
-                            <div></div>
-                            <span>Loadinssg...</span>
-                        </div>
-                    ) : (
-                        <div>
-                            {index === -1 ? (
-                                <div className="question--list">
-                                    {timer === 4 ? (
-                                        <>
-                                            <div> </div>
-                                            <span>READY?</span>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <div> </div>
-                                            <span>{timer}</span>
-                                        </>
-                                    )}
-                                </div>
-                            ) : null}
-                            {!finished ? (
-                                current && (
-                                    <>
-                                        <div className="question--list">
-                                            <div>
-                                                Question {index + 1} out of{" "}
-                                                {data.length} time left :{" "}
-                                                {timer}
-                                            </div>
-                                            <span>{current.question}</span>
-                                        </div>
-                                        <div className="question--image">
-                                            <Image
-                                                src={
-                                                    current.imgURL ||
-                                                    "/notfound.png"
-                                                }
-                                                width={300}
-                                                height={300}
-                                                alt={current.question}
-                                            />
-                                        </div>
-                                        <div className="question--choice">
-                                            <QustionList
-                                                data={current.choices}
-                                                setAnswer={setAnswer}
-                                            />
-                                        </div>
-                                    </>
-                                )
-                            ) : (
-                                <div className="question--finish">
-                                    <div>
-                                        <Image
-                                            src="/trophy.png"
-                                            alt="trophy"
-                                            layout="fill"
-                                        />
-                                    </div>
-                                    <h1>Well Done, Username !</h1>
-                                    <h2>Correct answer: ...</h2>
-                                    <h3>Point Gained: {score}</h3>
-                                    <h4>Rank: ...</h4>
-                                </div>
-                            )}
-                        </div>
-                    )}
-                </div>
-            </div>
+      <div className="playCard">
+        <span onClick={() => router.push(`/`)}>&#10006;</span>
+        <div className="playCard__header">
+          <h1>{detailLoading ? null : challengeDetail.name}</h1>
         </div>
-    );
+        <div className="playCard__body">
+          {loading ? (
+            <div className="question--list">
+              <div></div>
+              <span>Loadinssg...</span>
+            </div>
+          ) : (
+            <div>
+              {index === -1 ? (
+                <div className="question--list">
+                  {timer === 4 ? (
+                    <>
+                      <div> </div>
+                      <span>READY?</span>
+                    </>
+                  ) : (
+                    <>
+                      <div> </div>
+                      <span>{timer}</span>
+                    </>
+                  )}
+                </div>
+              ) : null}
+              {!finished ? (
+                current && (
+                  <>
+                    <div className="question--list">
+                      <div>
+                        Question {index + 1} out of {data.length} time left :{" "}
+                        {timer}
+                      </div>
+                      <span>{current.question}</span>
+                    </div>
+                    <div className="question--image">
+                      <Image
+                        src={current.imgURL || "/notfound.png"}
+                        width={300}
+                        height={300}
+                        alt={current.question}
+                      />
+                    </div>
+                    <div className="question--choice">
+                      <QustionList
+                        data={current.choices}
+                        setAnswer={setAnswer}
+                      />
+                    </div>
+                  </>
+                )
+              ) : (
+                <div className="question--finish">
+                  <div>
+                    <Image src="/trophy.png" alt="trophy" layout="fill" />
+                  </div>
+                  <h1>Well Done, @{user.displayName} !</h1>
+                  <h2>Correct answer: {correctness}</h2>
+                  <h3>Point Gained: {score}</h3>
+                  {/* <h4>Rank: ...</h4> */}
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function QustionList({ data, setAnswer }) {
-    return data.map((doc, idx) => (
-        <div key={idx} onClick={() => setAnswer(doc)}>
-            <div>{doc}</div>
-        </div>
-    ));
+  return data.map((doc, idx) => (
+    <div key={idx} onClick={() => setAnswer(doc)}>
+      <div>{doc}</div>
+    </div>
+  ));
 }
