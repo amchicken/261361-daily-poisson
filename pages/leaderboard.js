d1d5359f840b0d1075ad511581a28e8ea53ae0a0
ThisGameZR <timezazare@gmail.com>
2021-10-06 14:11:21 +0700

    leaderboard page

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -10,99 +10,133 @@ import toast from "react-hot-toast";
 const LIMIT = 5;
 
 export default function Leaderboard() {
-  const [firestoreData, dataLoading] = useCollectionDataOnce(
-    firestore.collection("usernames").orderBy("points", "desc").limit(LIMIT),
-    { idField: "id" }
-  );
-  const [loading, setLoading] = useState(true);
-  const [search, setSearch] = useState("");
-  const [serchData, setSerchData] = useState([]);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    setLoading(dataLoading);
-  }, [dataLoading]);
-
-  useEffect(() => {
-    if (firestoreData)
-      setData(
-        firestoreData.map((doc, index) => {
-          return { ...doc, index: index + 1 };
-        })
-      );
-  }, [firestoreData]);
-
-  const debounceFn = useCallback(_.debounce(handleDB, 1000), [data]);
-
-  async function handleDB(input) {
-    setLoading(true);
-    let target = data.filter(
-      (doc, index) =>
-        doc.name.toLowerCase().split(" ").includes(input.toLowerCase()) ||
-        doc.username.toLowerCase().split(" ").includes(input.toLowerCase()) ===
-          input
+    const [firestoreData, dataLoading] = useCollectionDataOnce(
+        firestore
+            .collection("usernames")
+            .orderBy("points", "desc")
+            .limit(LIMIT),
+        { idField: "id" }
     );
+    const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState("");
+    const [serchData, setSerchData] = useState([]);
+    const [data, setData] = useState([]);
 
-    if (target.length === 0) {
-      console.log("FIRESOTRE");
-      const ref = firestore
-        .collection("usernames")
-        .where("name", "==", input)
-        .where("username", "==", input);
-      target = (await ref.get()).docs.map((doc) => {
-        return { ...doc.data(), index: `${LIMIT}++` };
-      });
+    useEffect(() => {
+        setLoading(dataLoading);
+    }, [dataLoading]);
+
+    useEffect(() => {
+        if (firestoreData)
+            setData(
+                firestoreData.map((doc, index) => {
+                    return { ...doc, index: index + 1 };
+                })
+            );
+    }, [firestoreData]);
+
+    const debounceFn = useCallback(_.debounce(handleDB, 1000), [data]);
+
+    async function handleDB(input) {
+        setLoading(true);
+        let target = data.filter(
+            (doc, index) =>
+                doc.name
+                    .toLowerCase()
+                    .split(" ")
+                    .includes(input.toLowerCase()) ||
+                doc.username
+                    .toLowerCase()
+                    .split(" ")
+                    .includes(input.toLowerCase()) === input
+        );
+
+        if (target.length === 0) {
+            console.log("FIRESOTRE");
+            const ref = firestore
+                .collection("usernames")
+                .where("name", "==", input)
+                .where("username", "==", input);
+            target = (await ref.get()).docs.map((doc) => {
+                return { ...doc.data(), index: `${LIMIT}++` };
+            });
+        }
+
+        if (search === "" && target.length === 0) {
+            toast.error("NOT FOUND");
+        }
+
+        setLoading(false);
+        setSerchData(target);
     }
 
-    if (search === "" && target.length === 0) {
-      toast.error("NOT FOUND");
+    function handleChange(e) {
+        setSearch(e.target.value);
+        debounceFn(e.target.value);
     }
 
-    setLoading(false);
-    setSerchData(target);
-  }
-
-  function handleChange(e) {
-    setSearch(e.target.value);
-    debounceFn(e.target.value);
-  }
-
-  if (loading) return <div>Loading...</div>;
-  return (
-    <div className="container">
-      <Nav />
-      <div style={{ backgroundColor: "white" }}>
-        <input type="text" value={search} onChange={handleChange} />
-        <button onClick={() => setSearch("")}>Clear seach</button>
-        <div>
-          {serchData.length > 0
-            ? serchData.map((doc) => (
-                <div key={doc.id}>
-                  <Image
-                    src={doc.photoURL}
-                    width={30}
-                    height={30}
-                    alt="photoURL"
-                  />
-                  #{doc.index} {doc.username}
-                  {doc.points}
-                </div>
-              ))
-            : data.map((doc) => (
-                <div key={doc.id}>
-                  <Image
-                    src={doc.photoURL}
-                    width={30}
-                    height={30}
-                    alt="photoURL"
-                  />
-                  #{doc.index} {doc.username}
-                  {doc.points}
-                </div>
-              ))}
+    if (loading) return <div>Loading...</div>;
+    return (
+        <div className="container">
+            <Nav />
+            <div className="leaderboard">
+                <header>
+                    <h1>LEADERBOARD</h1>
+                </header>
+                <section>
+                    <div>
+                        <h4>SEARCH</h4>
+                        <input
+                            type="text"
+                            value={search}
+                            onChange={handleChange}
+                            placeholder="TYPE HERE"
+                        />
+                        <button onClick={() => setSearch("")}>
+                            RESET FILTER
+                        </button>
+                    </div>
+                    <div>
+                        <div>
+                            <div>P L A Y E R S</div>
+                            <div></div>
+                            <div>P O I N T S</div>
+                        </div>
+                        {serchData.length > 0
+                            ? serchData.map((doc) => (
+                                  <div key={doc.id}>
+                                      <div>#{doc.index}</div>
+                                      <div>
+                                          <Image
+                                              src={doc.photoURL}
+                                              width={30}
+                                              height={30}
+                                              alt="photoURL"
+                                          />
+                                          {doc.username}
+                                      </div>
+                                      <div>{doc.points}</div>
+                                  </div>
+                              ))
+                            : data.map((doc) => (
+                                  <div key={doc.id}>
+                                      <div>#{doc.index}</div>
+                                      <div>
+                                          <Image
+                                              src={doc.photoURL}
+                                              width={30}
+                                              height={30}
+                                              alt="photoURL"
+                                          />
+                                          {doc.username}
+                                      </div>
+                                      <div>{doc.points}</div>
+                                  </div>
+                              ))}
+                    </div>
+                </section>
+            </div>
+            <div className="container__footer">DAILYPOISSON 2021 | SITE</div>
         </div>
-      </div>
-      <div className="container__footer">DAILYPOISSON 2021 | SITE</div>
-    </div>
-  );
+    );
 }
