0e1f5e44f76459ba118a3f197ee25fe327133567
ThisGameZR <timezazare@gmail.com>
2021-10-07 03:02:30 +0700

    changes

--- components/Nav.js
+++ components/Nav.js
@@ -7,84 +7,97 @@ import { FaBars, FaSearch } from "react-icons/fa";
 import { auth } from "@lib/firebase";
 
 export default function Nav({ selected, setSelected }) {
-  const { user } = useContext(UserContext);
-  const router = useRouter();
-  const current = router.pathname.split("/")[1];
-  const [showLogOut, setShowLogOut] = useState(false);
+    const { user } = useContext(UserContext);
+    const router = useRouter();
+    const current = router.pathname.split("/")[1];
+    const [showLogOut, setShowLogOut] = useState(false);
 
-  return (
-    <nav className="nav">
-      <div className="nav__float show">
-        <div className="nav__float__top">
-          <Image
-            src={user.photoURL || "/notfound.png"}
-            width={100}
-            height={100}
-            alt="profile-image"
-          />
-          {showLogOut ? (
-            <div className="logout-box">
-              <div>
-                <div>@{user.username}</div>
-                <span>{user.email}</span>
-              </div>
-              <button onClick={() => auth.signOut()}>SIGN OUT</button>
-            </div>
-          ) : (
-            <span onClick={() => setShowLogOut(!showLogOut)}>
-              @{user.username}
-            </span>
-          )}
-          <div className="nav__float__top line"></div>
-        </div>
-        <ul className="nav__float__list" onClick={() => setShowLogOut(false)}>
-          <Link href="/" passHref>
-            <Image
-              src="/img/logo.png"
-              width="90"
-              height="64"
-              alt="logo"
-              className="pointer"
-            />
-          </Link>
+    return (
+        <nav className="nav">
+            <div className="nav__float show">
+                <div className="nav__float__top">
+                    <Image
+                        src={user.photoURL || "/notfound.png"}
+                        width={100}
+                        height={100}
+                        alt="profile-image"
+                    />
+                    {showLogOut ? (
+                        <div className="logout-box">
+                            <div>
+                                <div>@{user.username}</div>
+                                <span>{user.email}</span>
+                            </div>
+                            <button onClick={() => auth.signOut()}>
+                                SIGN OUT
+                            </button>
+                        </div>
+                    ) : (
+                        <span onClick={() => setShowLogOut(!showLogOut)}>
+                            @{user.username}
+                        </span>
+                    )}
+                    <div className="nav__float__top line"></div>
+                </div>
+                <ul
+                    className="nav__float__list"
+                    onClick={() => setShowLogOut(false)}
+                >
+                    <Link href="/" passHref>
+                        <Image
+                            src="/img/logo.png"
+                            width="90"
+                            height="64"
+                            alt="logo"
+                            className="pointer"
+                        />
+                    </Link>
 
-          <li
-            style={
-              current == "profile" ? { color: "#48f36e" } : { color: "white" }
-            }
-          >
-            <Link href="/profile" passHref>
-              PROFILE
-            </Link>
-          </li>
-          <li style={current == "" ? { color: "#48f36e" } : { color: "white" }}>
-            <Link href="/" passHref>
-              DAILY CHALLENGES
-            </Link>
-          </li>
-          <li
-            style={
-              current == "leaderboard"
-                ? { color: "#48f36e" }
-                : { color: "white" }
-            }
-          >
-            <Link href="/leaderboard" passHref>
-              LEADERBOARD
-            </Link>
-          </li>
-          <li
-            style={
-              current == "repository"
-                ? { color: "#48f36e" }
-                : { color: "white" }
-            }
-          >
-            <Link href="/repository" passHref>
-              REPOSITORY
-            </Link>
-          </li>
-          {/* <li
+                    <li
+                        style={
+                            current == "profile"
+                                ? { color: "#48f36e" }
+                                : { color: "white" }
+                        }
+                    >
+                        <Link href="/profile" passHref>
+                            PROFILE
+                        </Link>
+                    </li>
+                    <li
+                        style={
+                            current == ""
+                                ? { color: "#48f36e" }
+                                : { color: "white" }
+                        }
+                    >
+                        <Link href="/" passHref>
+                            DAILY CHALLENGES
+                        </Link>
+                    </li>
+                    <li
+                        style={
+                            current == "leaderboard"
+                                ? { color: "#48f36e" }
+                                : { color: "white" }
+                        }
+                    >
+                        <Link href="/leaderboard" passHref>
+                            LEADERBOARD
+                        </Link>
+                    </li>
+                    <li
+                        style={
+                            current == "repository"
+                                ? { color: "#48f36e" }
+                                : { color: "white" }
+                        }
+                    >
+                        <Link href="/repository" passHref>
+                            REPOSITORY
+                        </Link>
+                    </li>
+                    {/* <li
             onClick={() => setSelected("privateSession")}
             style={
               current == "privateSession"
@@ -94,16 +107,16 @@ export default function Nav({ selected, setSelected }) {
           >
             PRIVATE SESSION
           </li> */}
-          <li
+                    {/* <li
             style={
               current == "search" ? { color: "#48f36e" } : { color: "white" }
             }
           >
             <FaSearch />
-          </li>
-          <span>CONTACT | ABOUT | HELP CENTER |</span>
-        </ul>
-      </div>
-    </nav>
-  );
+          </li> */}
+                    <span>CONTACT | ABOUT | HELP CENTER |</span>
+                </ul>
+            </div>
+        </nav>
+    );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -6,18 +6,30 @@ import { Toaster } from "react-hot-toast";
 import { auth } from "@lib/firebase";
 
 function MyApp({ Component, pageProps }) {
-  const { user, loading } = useUser();
-  if (loading) return <div>..Loading...</div>;
-  else {
-    if (!auth) return <LoginRegister />;
-    if (!user) return <LoginRegister />;
-    return (
-      <UserContext.Provider value={{ user: user, loading: loading }}>
-        <Toaster />
-        <Component {...pageProps} />
-      </UserContext.Provider>
-    );
-  }
+    const { user, loading } = useUser();
+    if (loading) return <div>..Loading...</div>;
+    else {
+        if (!auth)
+            return (
+                <>
+                    <Toaster />
+                    <LoginRegister />
+                </>
+            );
+        if (!user)
+            return (
+                <>
+                    <Toaster />
+                    <LoginRegister />
+                </>
+            );
+        return (
+            <UserContext.Provider value={{ user: user, loading: loading }}>
+                <Toaster />
+                <Component {...pageProps} />
+            </UserContext.Provider>
+        );
+    }
 }
 
 export default MyApp;
