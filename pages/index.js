ecb26080e18745348463250fd619dfef1fa1c584
amchicken <yooradet.khamon@hotmail.com>
2021-10-06 21:56:19 +0700

    set default value on createChallenge

--- pages/index.js
+++ pages/index.js
@@ -5,58 +5,46 @@ import Repository from "@components/Repository";
 import { useState } from "react";
 
 export default function Home() {
-    const [subMenu, setSubMenu] = useState("home");
-    return (
-        <>
-            <div className="container">
-                <Nav />
-                <div className="container__right">
-                    <>
-                        <div className="container__right__header">
-                            <span
-                                className="pointer"
-                                style={
-                                    subMenu === "home"
-                                        ? { fontWeight: 600 }
-                                        : null
-                                }
-                                onClick={() => setSubMenu("home")}
-                            >
-                                HOME
-                            </span>
-                            <span
-                                className="pointer"
-                                style={
-                                    subMenu === "topplay"
-                                        ? { fontWeight: 600 }
-                                        : null
-                                }
-                                onClick={() => setSubMenu("topplay")}
-                            >
-                                TOP PLAY
-                            </span>
-                            <div>
-                                LOAD NEW CHALLENGES{"  "}
-                                <IoReload style={{ color: "#48f36e" }} />
-                            </div>
-                        </div>
-                        <div className="container__right__content">
-                            {subMenu === "home" ? <Repository /> : null}
-                            {subMenu === "topplay" ? (
-                                <Repository sort="play" />
-                            ) : null}
-                        </div>
-                        <div className="container__right__footer">
-                            <Link href="/Challenge/create" passHref>
-                                <button>CREATE YOUR CHALLENGE</button>
-                            </Link>
-                        </div>
-                    </>
-                </div>
-                <div className="container__footer">
-                    DAILYPOISSON 2021 | SITE
-                </div>
+  const [subMenu, setSubMenu] = useState("home");
+  return (
+    <>
+      <div className="container">
+        <Nav />
+        <div className="container__right">
+          <>
+            <div className="container__right__header">
+              <span
+                className="pointer"
+                style={subMenu === "home" ? { fontWeight: 600 } : null}
+                onClick={() => setSubMenu("home")}
+              >
+                HOME
+              </span>
+              <span
+                className="pointer"
+                style={subMenu === "topplay" ? { fontWeight: 600 } : null}
+                onClick={() => setSubMenu("topplay")}
+              >
+                TOP PLAY
+              </span>
+              <div>
+                LOAD NEW CHALLENGES{"  "}
+                <IoReload style={{ color: "#48f36e" }} />
+              </div>
             </div>
-        </>
-    );
+            <div className="container__right__content">
+              {subMenu === "home" ? <Repository /> : null}
+              {subMenu === "topplay" ? <Repository sort="play" /> : null}
+            </div>
+            <div className="container__right__footer">
+              <Link href="/challenge/create" passHref>
+                <button>CREATE YOUR CHALLENGE</button>
+              </Link>
+            </div>
+          </>
+        </div>
+        <div className="container__footer">DAILYPOISSON 2021 | SITE</div>
+      </div>
+    </>
+  );
 }
